82f16eab93ce35614250e0f92bee29c8e418bda7
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-23 04:34:26 +0330

    Fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center px-3 pt-36">
+        <main className="flex flex-col items-center px-3 pt-36 text-center">
             <h1 className="font-bold text-3xl lg:text-6xl tracking-tight">
                 Find events around you
             </h1>
-            <p className="opacity-75 mt-7 mb-12 text-lg lg:text-3xl text-center tracking-wide">
+            <p className="opacity-75 mt-7 mb-12 text-lg lg:text-3xl tracking-wide">
                 Browse more than{" "}
                 <span className="font-bold text-accent underline underline-offset-2 tracking-tighter">
                     10,000
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,7 +13,7 @@ const routes = [
 
 export default function Footer() {
     return (
-        <footer className="flex justify-between items-center mt-auto px-3 md:px-12 border-white/20 border-t h-14 text-white/50 text-xs md:text-base">
+        <footer className="flex justify-between items-center mt-auto px-3 sm:px-9 md:px-12 border-white/20 border-t h-14 text-white/50 text-xs md:text-sm">
             <p>&copy; 2077 NighCity. All rights reserved.</p>
 
             <ul className="flex sm:flex-row flex-col gap-x-3 sm:gap-x-8">
