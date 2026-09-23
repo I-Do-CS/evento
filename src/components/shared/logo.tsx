9163eaeb68bc897dd16a48a0d5a01c74c7a063f1
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-23 06:01:06 +0330

    Fix bug

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import logoImag from "@/assets/images/evento.png";
 import { DIMENSIONS } from "@/lib/constants";
@@ -6,5 +7,9 @@ import { DIMENSIONS } from "@/lib/constants";
 export default function Logo() {
     const { width, height } = DIMENSIONS.logo;
 
-    return <Image src={logoImag} alt="evento logo" width={width} height={height} />;
+    return (
+        <Link href="/" className="flex items-center min-h-full">
+            <Image src={logoImag} alt="evento logo" width={width} height={height} />
+        </Link>
+    );
 }
