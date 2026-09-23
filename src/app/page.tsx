f3dc6da938eb7cbd920cae7bbf55a32f081b307c
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-23 03:50:01 +0330

    Add markup and styling for the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,34 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Find events around you</h1>
-            <p>Browse more than 10,000 events around you</p>
+        <main className="flex flex-col items-center px-3 pt-36">
+            <h1 className="font-bold text-3xl lg:text-6xl tracking-tight">
+                Find events around you
+            </h1>
+            <p className="opacity-75 mt-7 mb-12 text-lg lg:text-3xl">
+                Browse more than{" "}
+                <span className="font-bold text-[#A4F839] underline underline-offset-2 italic">
+                    10,000
+                </span>{" "}
+                events around you
+            </p>
 
-            <form>
-                <input type="text" placeholder="Search events in any city..." spellCheck={false} />
+            <form className="w-full sm:w-[80%] md:w-[60%] lg:w-[40%] xl:w-[30%]">
+                <input
+                    className="bg-white/7 focus:bg-white/10 px-6 rounded-lg outline-none ring-[#A4F839]/50 focus:ring-2 w-full h-16 transition"
+                    type="text"
+                    placeholder="Search events in any city..."
+                    spellCheck={false}
+                />
             </form>
 
-            <section>
-                <p>Popular: </p>
-                <div>
+            <section className="flex gap-x-4 mt-4 text-white text-sm">
+                <p className="opacity-90">Popular: </p>
+                <div className="space-x-2 opacity-50 underline underline-offset-4">
                     <Link href="/events/austin">Austin</Link>
                     <Link href="/events/seattle">Seattle</Link>
                 </div>
             </section>
-        </div>
+        </main>
     );
 }
