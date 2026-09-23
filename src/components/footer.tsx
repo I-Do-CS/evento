319891652fd122cec4f539d39c479cfd631bdcda
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-23 04:57:38 +0330

    Add active link styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,7 +13,7 @@ const routes = [
 
 export default function Footer() {
     return (
-        <footer className="flex justify-between items-center mt-auto px-3 sm:px-9 md:px-12 border-white/20 border-t h-14 text-white/50 text-xs md:text-sm">
+        <footer className="flex justify-between items-center mt-auto px-3 sm:px-9 md:px-12 border-white/20 border-t h-10 md:h-14 text-white/50 text-xs md:text-sm">
             <p>&copy; 2077 NighCity. All rights reserved.</p>
 
             <ul className="flex sm:flex-row flex-col gap-x-3 sm:gap-x-8">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
     { name: "home", path: "/" },
@@ -7,15 +12,31 @@ const routes = [
 ];
 
 export default function Header() {
+    const activePath = usePathname();
+
     return (
-        <header className="flex justify-between items-center px-3 sm:px-9 border-white/20 border-b h-14 tracking-tight">
+        <header className="flex justify-between items-center px-3 sm:px-9 border-white/20 border-b h-10 md:h-14 tracking-tight">
             <Logo />
 
-            <nav>
-                <ul className="flex gap-3 md:gap-x-6 text-white/70 text-sm capitalize">
+            <nav className="h-full">
+                <ul className="flex justify-center gap-2 md:gap-x-4 h-full text-white/70 text-sm capitalize">
                     {routes.map((route) => (
-                        <li key={route.path} className="hover:text-white transition">
+                        <li
+                            className={clsx(
+                                "relative flex justify-center items-center px-1 h-full hover:text-white transition",
+                                {
+                                    "text-white": activePath === route.path,
+                                },
+                            )}
+                            key={route.path}
+                        >
                             <Link href={route.path}>{route.name}</Link>
+                            {activePath === route.path && (
+                                <motion.div
+                                    layoutId="header-active-link"
+                                    className="bottom-0 absolute bg-accent w-full h-1"
+                                ></motion.div>
+                            )}
                         </li>
                     ))}
                 </ul>
